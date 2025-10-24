505feffab7313f4d80d834d0e4655f46b71ab234
rifqimhmd <rifqimuh390@gmail.com>
2025-10-24 16:23:15 +0700

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-gray-800 text-gray-300 text-sm py-4 mt-10">
     <div class="container mx-auto text-center">
-        &copy; {{ date('Y') }} Klinik Patnal. Semua hak cipta dilindungi.
+        &copy; {{ date('Y') }} Klinik Patnal. Semua hak cipta dilindungi Ditjenpas.
     </div>
 </footer>
